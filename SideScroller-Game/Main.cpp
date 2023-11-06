8a65cbbfbe7683246829e401c3bcc93f52423f35
gueris arriaza <gueris01@hotmail.com>
2023-11-05 19:11:46 -0500

    Creatd sprite and allow player to move

--- SideScroller-Game/Main.cpp
+++ SideScroller-Game/Main.cpp
@@ -1,16 +1,85 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int windowWidth = 200;
-int windowHeight = 400;
+int windowWidth = 900;
+int windowHeight = 900;
+int halgwWinHeight = windowHeight / 2;
+int halgwWinWidth = windowWidth / 2;
 
 using namespace sf;
 using namespace std;
 
-class playerClass{
+class platformClass {
     public:
+        float xvel;
+        float yvel;
+        float xpos;
+        float ypos;
+        platformClass(float xpos, float ypos) {
+            xpos = xpos;
+            ypos = ypos;
+        }
+
+};
+class playerClass{
+    
+    public:
+        float xvel;
+        float yvel;
+        float xpos;
+        float ypos;
+        bool playerFaceright;
+        bool onGround;
         playerClass(){
-            cout << "Hello World" << endl;
+            playerFaceright = true;
+            onGround = false;
+            xvel = 0;
+            yvel = 0;
+            xpos = 0;
+            ypos = 0;
+            
+        }
+        void update(bool playerUp,bool playerDown,bool playerLeft,bool playerRight) {
+            if (playerRight) {
+                playerFaceright = true;
+                xvel = 1;
+            }
+            else if (playerLeft) {
+                playerFaceright = false;
+                xvel = -1;
+            }
+            if (playerUp) {
+                yvel = -1;
+            }
+            else if (playerDown) {
+                yvel = 1;
+            }
+            if (!(playerRight || playerLeft)) {
+                
+                xvel = 0;
+            }
+            if (!(playerUp || playerDown)) {
+
+                yvel = 0;
+            }
+            if (onGround) {
+                yvel = 0;
+            }
+            xpos += xvel;
+            ypos += yvel;
+            collide();
+
+            cout << xpos << endl;
+            cout << ypos << endl;
+        }
+        void collide() {
+            if (ypos > 100) {
+                yvel = 0;
+                onGround = true;
+            }
+            else {
+                onGround = false;
+            }
         }
 };
 
@@ -21,7 +90,11 @@ int main()
     bool playerUp = false, playerDown = false, playerLeft = false, playerRight = false;
 
 
-    playerClass playeObject;
+    playerClass playerObject ;
+
+    Texture alienTexture;
+    alienTexture.loadFromFile("C:\\Users\\gueri\\OneDrive\\Programming\\SideScroller-Game\\Data\\Images\\Alien.png");
+    Sprite alienSprite(alienTexture);
 
     while (window.isOpen())
     {
@@ -42,12 +115,14 @@ int main()
         if (!(Keyboard::isKeyPressed(Keyboard::Up))) playerUp = false;
         if (!(Keyboard::isKeyPressed(Keyboard::Down))) playerDown = false;
 
-        cout << playerDown << endl;
-        cout << playerUp << endl;
-        cout << playerLeft << endl;
-        cout << playerRight << endl;
+        playerObject.update(playerUp, playerDown, playerLeft, playerRight);
+
+        
 
         window.clear();
+
+        window.draw(alienSprite);
+        alienSprite.move(Vector2f(playerObject.xvel, playerObject.yvel));
         
         window.display();
         
